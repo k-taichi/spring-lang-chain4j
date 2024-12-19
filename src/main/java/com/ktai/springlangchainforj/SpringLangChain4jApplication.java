ebb54b9645adf9374b3db6237d4d4a960d3c6e3e
taichi-kaneko <taichi-kaneko@gmogshd.com>
2024-12-19 17:35:04 +0900

    first commit

---
+++ src/main/java/com/ktai/springlangchainforj/SpringLangChain4jApplication.java
@@ -0,0 +1,13 @@
+package com.ktai.springlangchainforj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLangChain4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLangChain4jApplication.class, args);
+    }
+
+}
